d2c9545a9b2083464d098ee95e092cf4a362fcc9
Muhammad Rameez <32459297+MRameez820@users.noreply.github.com>
2022-07-30 20:40:19 +0500

    Update our-vehicles.js

--- our-vehicles.js
+++ our-vehicles.js
@@ -308,7 +308,7 @@ if(singval.vehicle_class_id == $('.van-class-id')[x].innerHTML)
 if(singval.availability.quantity > 0) {
 var agecheck = age == 'On';
 
-$('.price-amount')[x].innerHTML =  '£' + $('#age').val() == '25' ? singval.price.base_price_with_taxes.amount.replace('.',',') : '£' +  (singval.price.base_price_with_taxes.amount + young_driver_cost).toFixed(2).replace('.',',');
+$('.price-amount')[x].innerHTML =  '£' + $('#age').val() == '25' ? singval.price.base_price_with_taxes.amount.replace('.',',') : '£' +  Number(singval.price.base_price_with_taxes.amount + young_driver_cost).toFixed(2).replace('.',',');
 //'.price-amount')[x].innerHTML =  '£' +  agecheck ? singval.price.base_price_with_taxes.amount_for_display : Number(singval.price.base_price_with_taxes.amount_for_display.slice(1).replace(/,/g, '.')) + Number(young_driver_cost) ;
 //make change in first condition
 //$('.price-amount')[x].innerHTML =  '£' +  agecheck ? 25 : 50;
