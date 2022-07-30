0c08e5f866e9ca3f09b6cfa925f3dddf0a6776ab
Muhammad Rameez <32459297+MRameez820@users.noreply.github.com>
2022-07-30 14:27:18 +0500

    Update our-vehicles.js

--- our-vehicles.js
+++ our-vehicles.js
@@ -307,8 +307,9 @@ if(singval.vehicle_class_id == $('.van-class-id')[x].innerHTML)
 {
 if(singval.availability.quantity > 0) {
 var agecheck = age == 'On';
-//$('.price-amount')[x].innerHTML =  '£' +  agecheck ? singval.price.base_price_with_taxes.amount_for_display : Number(singval.price.base_price_with_taxes.amount_for_display.slice(1).replace(/,/g, '.')) + Number(young_driver_cost) ;
-$('.price-amount')[x].innerHTML =  '£' +  agecheck ? 25 : 50;
+$('.price-amount')[x].innerHTML =  '£' +  agecheck ? singval.price.base_price_with_taxes.amount_for_display : Number(singval.price.base_price_with_taxes.amount_for_display.slice(1).replace(/,/g, '.')) + Number(young_driver_cost) ;
+//make change in first condition
+//$('.price-amount')[x].innerHTML =  '£' +  agecheck ? 25 : 50;
 var days = singval.price.total_days;
 var paramstr = addparam();
 let butturl =document.getElementsByClassName('card-button')[x].href.split("?")[0];
