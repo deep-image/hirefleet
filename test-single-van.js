e1240f98d0aabd0c3244520b01e7d339f506bed0
Muhammad Rameez <32459297+MRameez820@users.noreply.github.com>
2022-08-10 15:19:20 +0500

    Update test-single-van.js

--- test-single-van.js
+++ test-single-van.js
@@ -548,7 +548,6 @@ bothpicker =  new Litepicker({
 				},
 		setup: (bothpicker) => {
   bothpicker.on('selected', (date1, date2) => {
-console.log("both picker chng")	  
 picker.setDateRange(date1,date2)
   })
 	},		
