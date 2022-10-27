69d0dfd396a74e23bee3d30ab48848c79fdec31e
Muhammad Rameez <32459297+MRameez820@users.noreply.github.com>
2022-10-27 18:23:00 +0500

    Update test-single-van.js

--- test-single-van.js
+++ test-single-van.js
@@ -393,7 +393,7 @@ $('.summ-div-child').append('<div class="summ-extras-div"><div class="extras-tex
 })	
 data.selected_additional_charges.length > 0 ? $('.summary-div').show() : '';
 $('.summ-extras-div > .extras-text').length == 0 ? $('.summary-div').hide() : '';	    
-//agechangeonsimilar();
+agechangeonsimilar();
 	    //////////////////////this needs sorting/////////////////
 
 //$('#subtotal, #subtotal-mobile').text(data.selected_vehicle_class.price.base_price_with_taxes.amount_for_display);
@@ -419,21 +419,23 @@ $('#discount').val().length  > 0 ? appdisc(data) : '';
 function agechangeonsimilar()
 	{
 	var pricearr = document.getElementsByClassName('price-amount');
-if($('#age-range').val() == '25') {
-for(var x=0; x < pricearr.length; x++) {
-var tot = Number(pricearr[x].innerText.slice(1).replaceAll(',','.')) 
-pricearr[x].innerText = '£' + tot.toFixed(2);
-addageparam(x);
-}
-}
-else
-{
-for(var x=0; x < pricearr.length; x++) {
-addageparam(x);
-var tot = Number(pricearr[x].innerText.slice(1).replaceAll(',','.')) + Number(young_driver_cost)
-pricearr[x].innerText = '£' + tot.toFixed(2);
-}
-}
+	if($('.price-amount').eq(0).text() != '-----') {		
+		if($('#age-range').val() == '25') {
+		for(var x=0; x < pricearr.length; x++) {
+		var tot = Number(pricearr[x].innerText.slice(1).replaceAll(',','.')) 
+		pricearr[x].innerText = '£' + tot.toFixed(2);
+		addageparam(x);
+		}
+		}
+		else
+		{
+		for(var x=0; x < pricearr.length; x++) {
+		addageparam(x);
+		var tot = Number(pricearr[x].innerText.slice(1).replaceAll(',','.')) + Number(young_driver_cost)
+		pricearr[x].innerText = '£' + tot.toFixed(2);
+		}
+		}
+	}	
 	}	
 
 
@@ -766,6 +768,7 @@ $('#start-time, #end-time').change(function() {
 });
 
 $('#start-time, #end-time, #age-range').change(function() {
+	
 	setTimeout(function() { 
 //	console.log($('#age-range').val() == '25' ? 1 : '')	
 	subdetectchange()
