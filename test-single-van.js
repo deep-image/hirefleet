63c2a554d6b98a72dfa680a3a9b9df2c0a131807
deep-image <65763307+deep-image@users.noreply.github.com>
2022-11-16 20:18:00 +0000

    Update test-single-van.js

--- test-single-van.js
+++ test-single-van.js
@@ -177,7 +177,7 @@ window.cc_c2a.addEmailVerify({
 
 window.cc_c2a.addPhoneVerify({
 phone: '#phone'	,
-country : '#addr_country'	
+country : 'United Kingdom'	
 })
 
 // change background colour of dropdown
