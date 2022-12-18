7d09057bc28e8828982c421861440866511bade7
Muhammad Rameez <32459297+MRameez820@users.noreply.github.com>
2022-12-18 10:09:52 +0500

    Update test-single-van.js

--- test-single-van.js
+++ test-single-van.js
@@ -960,9 +960,11 @@ getsinglevan();
 
 function setupunavailable(vehicledata)
 	{
+	console.log( vehicledata )	
 	vehicledata.forEach((singval,x) => { 
 if(singval.vehicle_class_id == $('#class-id').text()) {
 vanavailable = singval.availability.quantity == 0;
+console.log(vanavailable)	
 vanavailable == true ? $('#booking-butt').val('Check Availability') :  $('#booking-butt').val('confirm & pay');document.getElementsByClassName('available')[x+1].style.display = 'block';
 	if(vanavailable == true){$('#Summary').text("Your enquiry summary");$('#Complete').text("Complete your enquiry");document.getElementsByClassName('limited')[x+1].style.display = 'block';}
 }
