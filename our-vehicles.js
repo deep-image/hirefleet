0805614cc24ff89f9dd6270d435b265f81749a51
Muhammad Rameez <32459297+MRameez820@users.noreply.github.com>
2023-09-21 17:30:19 +0500

    Update our-vehicles.js

--- our-vehicles.js
+++ our-vehicles.js
@@ -273,7 +273,7 @@ var res = data.data;
 applypricing(res.applicable_classes);
 //setting available sort if selected before	
 	for(x=0; x<2; x++) {		
-	$('#van-sort').val() == 'Available' ? $('#avail-sort-butt')[0].click() : '';			
+	$('#van-sort').val() == 'available' ? $('#avail-sort-butt')[0].click() : '';			
 	$('#van-sort').val() == 'price-desc' ? $('#price-sort-butt')[0].click() : '';
 	$('#van-sort').val() == 'price-asc' ? $('#price-sort-butt')[0].click() : '';	
 	}
