6239cad62b455fe5f2bd007afcb02549db76465e
Muhammad Rameez <32459297+MRameez820@users.noreply.github.com>
2023-09-21 19:32:43 +0500

    Update our-vehicles.js

--- our-vehicles.js
+++ our-vehicles.js
@@ -85,6 +85,7 @@ if (window.location.href.indexOf("start-date") > -1) {
 $('#both-date').length > 0 ?  $('#both-date').val($('#start-date').val() + " - " + $('#end-date').val()) : '';		
 	 setTimeout(function() { 
 	if( getParam("sort")  == 'available' ) {
+	console.log(getParam("sort"))	
 	$('#van-sort').data('pre' , getParam("sort") ) 	 
 	$('#van-sort').val( getParam("sort")  );
 	$('#van-sort').change();	
