249159522585f6b965d22c8f1b93b0a2de7a614b
Muhammad Rameez <32459297+MRameez820@users.noreply.github.com>
2023-09-24 20:15:19 +0500

    Update our-vehicles.js

--- our-vehicles.js
+++ our-vehicles.js
@@ -342,6 +342,7 @@ for(x=loopcount; x< lckcount; x++) {
 bothpicker.setDateRange(nextavail,tomnextavail)	
 $('#end-time').val() == null ? $('#end-time').val($('#end-time option:first').val()) : ''	
 $('#start-time').val($('#start-time option:first').val()).change();
+console.log('next avail start chagne')	
 }
 
 
