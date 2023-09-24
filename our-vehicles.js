ebb2cb457598dd7436eacd19ccfd2418b3e03686
Muhammad Rameez <32459297+MRameez820@users.noreply.github.com>
2023-09-24 14:04:47 +0500

    Update our-vehicles.js

--- our-vehicles.js
+++ our-vehicles.js
@@ -139,6 +139,7 @@ var strt = $('#start-time :selected').val();
  console.log("selection change"); 
 if($('#start-time').val() != null && $('#end-time').val() != null) {
   setTimeout( function() {	
+sortcount = 0;	  
  makecall(date1.toDateString(),date2.toDateString(),strt,endt,age);  
    	   }, 300);
   }
@@ -339,8 +340,6 @@ for(x=loopcount; x< lckcount; x++) {
 } 
  }	  
 
-sortcount = 0;	
-console.log(sortcount)	
 bothpicker.setDateRange(nextavail,tomnextavail)	
 $('#end-time').val() == null ? $('#end-time').val($('#end-time option:first').val()) : ''	
 $('#start-time').val($('#start-time option:first').val()).change();
