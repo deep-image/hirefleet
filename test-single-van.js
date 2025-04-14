7ca2725419e757afe1ecb875a0d3b902e1663afd
Muhammad Rameez <32459297+MRameez820@users.noreply.github.com>
2025-04-14 19:47:14 +0500

    Update test-single-van.js

--- test-single-van.js
+++ test-single-van.js
@@ -404,9 +404,9 @@ if(data.length != 0) {
 
 	data.selected_additional_charges.forEach((singval,index) => { 
 	young_driver_cost = singval.id == 3  ? Number(singval.total_price_with_taxes.amount) : 0;  	
-	let selquan = singval.selected_quantity == 0 ? '' :  singval.selected_quantity;	
+	let quanclass = singval.selected_quantity == 0 ? 'extra-quant hidden-quan' :  'extra-quant';	
 	let extraprice =  Intl.NumberFormat('en-US', {  style: 'currency',  currency: 'GBP', minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(singval.total_price_with_taxes.amount);	
-	$('.summ-div-child').append('<div class="summ-extras-div"><div class="extras-text">'+singval.label+'<span class="extra-id" style="display:none">'+singval.id +'</span> <span class="extra-quant">x'+  singval.selected_quantity +' </span></div><div class="extras-pricing">'+extraprice+'</div></div>')
+	$('.summ-div-child').append('<div class="summ-extras-div"><div class="extras-text">'+singval.label+'<span class="extra-id" style="display:none">'+singval.id +'</span> <span class='+quanclass+'>x'+  singval.selected_quantity +' </span></div><div class="extras-pricing">'+extraprice+'</div></div>')
 	})	
 	data.selected_additional_charges.length > 0 ? $('.summary-div').show() : '';
 	$('.summ-extras-div > .extras-text').length == 0 ? $('.summary-div').hide() : '';	    
